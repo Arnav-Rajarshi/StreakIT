fe7ceccacd936c126bcb273f5495ea01b302a4f8
Arnav_Rajarshi <arnavrajarshi@gmail.com>
2026-07-05 22:47:09 +0530

    created a backend folder with three layer architecture . Coding around FastAPI endpoints . Added three types into models.py

--- app_functions.py
+++ app_functions.py
@@ -6,30 +6,19 @@ def validate_user(password: str, user_name: str = None, email: str = None) -> bo
     with db.get_connection() as conn:
         with conn.cursor() as cur:
             if user_name:
-                user = crud_db.get_UserDetails(user_name=user_name)
+                user = crud_db.read_UserDetails(user_name=user_name)
             elif email:
-                user = crud_db.get_UserDetails(email=email)
+                user = crud_db.read_UserDetails(email=email)
             else:
                 return False
             if user and auth.verify_password(password, user.encrypted_password):
                 return True
     return False
+
 """
 print(validate_user("password123", user_name="mock1"))
 print(validate_user("password456", email="mock2@example.com"))
 """
-
-
-def get_HabitConfig(uid, HabitName:str):
-    return crud_db.get_HabitConfig(uid, HabitName)
-
-
-def get_HabitCache(hid):
-    return crud_db.get_HabitCache(hid)
-
-
-
-
 
 username="mock1"
 password="password123"
@@ -38,17 +27,17 @@ habit_name = "Daily Reading"
 def main():
     if validate_user(password, user_name=username):
         global user
-        user=crud_db.get_UserDetails(user_name=username)
+        user=crud_db.read_UserDetails(user_name=username)
         print(f"User {username} validated successfully.")
         global habit_config
-        habit_config = get_HabitConfig(user.uid, habit_name)
+        habit_config = crud_db.read_HabitConfig(user.uid, habit_name)
 
         if habit_config:
 
             print(f"Habit Config for '{habit_name}': {habit_config}")
             print("-*-"*25)
             
-            habit_cache = get_HabitCache(habit_config.hid) 
+            habit_cache = crud_db.read_HabitCache(habit_config.hid) 
             if habit_cache:
                print(f"Habit Cache for '{habit_name}': {habit_cache}")
             else:
@@ -61,5 +50,5 @@ def main():
         print(f"User {username} validation failed.")
 
 main()
-print("-*-"*25)
-get_HabitCache(habit_config.hid)
+
+
---
+++ backend/database/crud.py
@@ -0,0 +1,113 @@
+from uuid import UUID
+from database.db import get_connection
+import psycopg
+import models
+
+def read_HabitConfig(uid, HabitName:str)->models.HabitConfig:
+    with get_connection() as conn: 
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+            cur.execute(
+                "SELECT hid, user_id, habit_name, icon, color, track_on, minimum_days, target_duration_per_session, is_active, created_at FROM habit_config WHERE user_id = %s AND habit_name = %s",
+                (uid, HabitName)
+            )
+            result=cur.fetchone()
+            if result:
+                return models.HabitConfig(**result)
+    return None
+
+
+def read_HabitCache(hid:UUID)->models.HabitCache:
+    with get_connection() as conn:
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+            cur.execute(
+                "SELECT habit_id, current_streak, best_streak, total_sessions, total_duration, last_completed_date, last_cache_update, curr_streak_start_date FROM habit_cache WHERE habit_id = %s",
+                (str(hid),)
+            )
+            result=cur.fetchone()
+            if result:
+                return models.HabitCache(**result)
+    return None
+
+
+def read_UserDetails(user_name=None , email=None)->models.User:
+    with get_connection() as conn:
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+            if user_name:
+                cur.execute(
+                    "SELECT * FROM users WHERE user_name = %s",
+                    (user_name,)
+                )
+            elif email:
+                cur.execute(
+                    "SELECT * FROM users WHERE email = %s",
+                    (email,)
+                )
+            else:
+                return None
+            
+            result=cur.fetchone()
+            #print(result)
+            if result:
+                return models.User(**result)
+    return None
+
+
+def create_HabitConfigAndCache(HabitConfig: models.HabitConfig):
+    '''This function inserts habit_config into the database.Also insert a corresponding habit_cache entry with default values '''
+
+    with get_connection() as conn:
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+
+            data = HabitConfig.model_dump()  # Convert the HabitConfig object to a dictionary
+            cur.execute(
+                """ 
+                INSERT INTO habit_config 
+                (
+                    hid, 
+                    user_id, 
+                    habit_name, 
+                    icon, 
+                    color, 
+                    track_on, 
+                    minimum_days, 
+                    target_duration_per_session, 
+                    is_active, 
+                    created_at
+                ) 
+                VALUES 
+                (
+                    %(hid)s, 
+                    %(user_id)s, 
+                    %(habit_name)s, 
+                    %(icon)s, %(color)s, 
+                    %(track_on)s, 
+                    %(minimum_days)s, 
+                    %(target_duration_per_session)s, 
+                    %(is_active)s, %(created_at)s
+                )
+                """
+                ,data
+            ) 
+            # psycopg natively maps the dicts key value pairs with its corresponding columns 
+
+            # inserting default row in habit_cache for the newly created habit_config
+            cur.execute(
+                "INSERT INTO habit_cache (habit_id, current_streak, best_streak, total_sessions, total_duration, last_completed_date, curr_streak_start_date) VALUES (%(habit_id)s, %(current_streak)s, %(best_streak)s, %(total_sessions)s, %(total_duration)s, %(last_completed_date)s, %(curr_streak_start_date)s)",
+                {
+                    "habit_id":data['hid'],
+                    "current_streak": 0,
+                    "best_streak": 0,
+                    "total_sessions": 0,
+                    "total_duration": 0,
+                    "last_completed_date": None,
+                    # database will insert last_cache_update as current timestamp by default 
+                    "curr_streak_start_date": None
+                }
+            )
+        conn.commit()
+
+def update_HabitCache(habit_cache: models.HabitCache):
+    ...
+
+
+
